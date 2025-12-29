b80daec3e6edc8da0347165c5aa5384af3ee92b0
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-12-28 21:28:24 -0800

    implement localstorage to store todos data

--- src/Context/TodosProvier.tsx
+++ src/Context/TodosProvier.tsx
@@ -1,73 +1,76 @@
-import {  useState } from "react";
-import type { ChildrenType, Todo, } from "../Common/type";
+import { useState } from "react";
+import type { ChildrenType, Todo } from "../Common/type";
 import { TodosContext } from "./TodosContext";
 
+export const TodosProvier = ({ children }: ChildrenType) => {
+  const [todos, setTodos] = useState<Todo[]>(() => {
+      try {
+         const newTodos = localStorage.getItem('todos') || [] ;
+         return JSON.parse(newTodos) as Todo[];
+      } catch (error) {
+          console.log(error);
+          
+            return []
+      }
+  });
 
+// add new todo
 
+  const handleAddTodo = (task: string) => {
+    setTodos((prev) => {
+      const newTodos: Todo[] = [
+        {
+          id: Math.random().toString(),
+          task: task,
+          completed: false,
+          createdAt: new Date(),
+        },
+        ...prev,
+      ];
+          localStorage.setItem('todos',JSON.stringify(newTodos))
+      return newTodos;
+    });
+  };
 
+  console.log("current value : ", todos);
 
-
-export const TodosProvier = ({children}:ChildrenType) => {
-      const [todos,setTodos] = useState<Todo[]>([])
-
-       
-
-       const handleAddTodo = (task:string) => {
-                        setTodos((prev) => {
-                             const newTodos:Todo[] = [
-                                 {
-                                    id : Math.random().toString(),
-                                    task : task,
-                                    completed : false,
-                                    createdAt : new Date()
-                                 },
-                                 ...prev
-                                 
-                                ]
-                                console.log("prev value : ",prev);
-                                return newTodos;
-                        })
-       }
-
-       console.log("current value : ",todos);
-
-
-       // ! Toggle todo 
- const  toggleTodoAsCompleted = (id:string) => {
-        setTodos((prev) => {
-           const newTodos = prev.map(todo => {
-                 if (todo.id === id ) {
-                  return {...todo,completed:!todo.completed}
-                 }
-                 return todo
-           })
-            return newTodos
-        })
- }
-
-//  delete todo
- const handleDelteTodo = (id:string) => {
-      setTodos((prev) => {
-        const newTodos = prev.filter(todo => todo.id !=id)
-         return newTodos;
-       })
- }
-
-
-        const provierInfo = {
-             todos,
-            //  setTodos,
-             handleAddTodo,
-             toggleTodoAsCompleted,
-             handleDelteTodo
+  // ! Toggle todo
+  const toggleTodoAsCompleted = (id: string) => {
+    setTodos((prev) => {
+      const newTodos = prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
         }
+        return todo;
+      });
+                localStorage.setItem('todos',JSON.stringify(newTodos))
+
+
+      return newTodos;
+    });
+  };
+
+  //  delete todo
+  const handleDelteTodo = (id: string) => {
+    setTodos((prev) => {
+      const newTodos = prev.filter((todo) => todo.id != id);
+                localStorage.setItem('todos',JSON.stringify(newTodos))
+
+      return newTodos;
+    });
+  };
+
+  const provierInfo = {
+    todos,
+    //  setTodos,
+    handleAddTodo,
+    toggleTodoAsCompleted,
+    handleDelteTodo,
+  };
 
   return (
     <TodosContext.Provider value={provierInfo}>
-
-        {children}
+      {children}
     </TodosContext.Provider>
-  )
-}
-
-
+  );
+};
